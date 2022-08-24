2face22e4a690fde6613573f959c0ce030df9818
claraaavieira <110980134+claraaavieira@users.noreply.github.com>
2022-08-24 09:38:58 -0300

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,12 @@
+def insertion_sort(lista):
+    n = len(lista)
+    for i in range(1, n):
+        chave = lista[i]
+        j = i - 1
+        while j >= 0 and lista[j] > chave:
+            lista[j+1] = lista[j]
+            j = j - 1
+        lista[j+1] = chave
+lista = [61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99, 101, 103, 105, 107, 109, 111, 113, 105, 107, 109]
+insertion_sort(lista)
+print(lista)
